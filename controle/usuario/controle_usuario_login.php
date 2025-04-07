bc913e857525fff874964a1b5dcb23024b6046d2
fsbiscaro <fsbiscaro@hotmail.com>
2025-04-07 12:27:41 -0300

    adicionando verificações de token às chamadas do usuário.

--- controle/usuario/controle_usuario_login.php
+++ controle/usuario/controle_usuario_login.php
@@ -13,6 +13,7 @@ $objUsuario = new Usuario();
 $objUsuario->setEmail_usuario( $objJson->usuario->email_usuario);
 $objUsuario->setSenha_usuario( $objJson->usuario->senha_usuario);
 
+//verificações padrões para login
 if($objUsuario->getEmail_usuario()==""){
     $objResposta->cod=1;
     $objResposta->status=false;
@@ -22,22 +23,30 @@ if($objUsuario->getEmail_usuario()==""){
     $objResposta->status=false;
     $objResposta->msg="A senha não pode ser vazia.";
 }else{
+    //chamando o método de login e validando se ele retornou verdadeiro
     if($objUsuario->login()==true){
-        $tokenJWT = new MeuTokenJWT();
-        $objClaimsToken = new stdClass();
+        $tokenJWT = new MeuTokenJWT(); //atribuindo o meu token à classe JWT
+        $objClaimsToken = new stdClass(); //criando um objeto genérico para armazenar as informações do token
+
+        //listando as informações desejadas no token
         $objClaimsToken->email_usuario = $objUsuario->getEmail_usuario();
         $objClaimsToken->id_usuario = $objUsuario->getId_usuario();
         $objClaimsToken->nome_usuario = $objUsuario->getNome_usuario();
         $objClaimsToken->cpf_usuario = $objUsuario->getCpf_usuario();
         $objClaimsToken->data_nascimento_usuario = $objUsuario->getData_nascimento_usuario();
         $objClaimsToken->telefone_usuario = $objUsuario->getTelefone_usuario();
+
+        //gerando o novo token
         $novoToken = $tokenJWT->gerarToken($objClaimsToken);
+
+        //retornando a resposta de sucesso após gerar o token
         $objResposta->cod = 3;
         $objResposta->msg = "Login efetuado com sucesso";
         $objResposta->status = true;
         $objResposta->usuario = $objUsuario;
         $objResposta->token = $novoToken;
     }else{
+        //retornando a resposta de erro caso o login retorne erro
         $objResposta->cod = 4;
         $objResposta->msg = "Erro ao efetuar o login.";
         $objResposta->status = false;
--- controle/usuario/controle_usuario_read_by_id.php
+++ controle/usuario/controle_usuario_read_by_id.php
@@ -1,36 +1,53 @@
 <?php
+use Firebase\JWT\MeuTokenJWT;
 
 require_once ("modelo/banco.php");
 require_once ("modelo/usuario.php");
+require_once ("modelo/MeuTokenJWT.php");
 
 $objResposta = new stdClass();
 $objUsuario = new Usuario();
-$objUsuario->setId_usuario($id_usuario);
 
-$vetor = $objUsuario->readById();
+$headers = getallheaders(); //recuperando os headers
+$authorization = $headers['Authorization']; //recuperando o token nos headers
+$meuToken = new MeuTokenJWT(); //atribuindo o meu token à classe de token
 
+//validando o meu token
+if($meuToken->validarToken($authorization)==true){
+    $payloadRecuperado = $meuToken->getPayload(); //recuperando payload (dados) do token
+    $objUsuario->setId_usuario($id_usuario);
+    $vetor = $objUsuario->readById();
 
-if($vetor==null){
-    http_response_code(404);
-    $objResposta->status == false;
-    echo json_encode(["erro" => "Usuário não encontrado"]);
-    return null;
-} else {
-    $objResposta->cod = 1;
-    $objResposta->status = true;
-    $objResposta->msg = "Usuário desejado";
-    $objResposta->usuario = $vetor;
-}
-
-header("Content-Type: application/json");
-
-if($objResposta->status == true){
-    header("HTTP:1.1 201");
+    if($vetor==null){
+        http_response_code(404);
+        $objResposta->status == false;
+        echo json_encode(["erro" => "Usuário não encontrado"]);
+        return null;
+    } else {
+        $objResposta->cod = 1;
+        $objResposta->status = true;
+        $objResposta->msg = "Usuário desejado";
+        $objResposta->usuario = $vetor;
+        $objResposta->token = $meuToken->gerarToken($payloadRecuperado); //gerando um novo token para que o usuário continue navegando pela aplicação
+    }
+    
+    header("Content-Type: application/json");
+    
+    if($objResposta->status == true){
+        header("HTTP:1.1 201");
+    }else{
+        header("HTTP:1.1 200");
+    }
+    
+    echo json_encode($objResposta);
 }else{
-    header("HTTP:1.1 200");
+    //retornando mensagem de token inválido caso o token não seja válido
+    header("Content-Type: application/json");
+    header("HTTP:1.1 401");
+    $objResposta->code = 2;
+    $objResposta->msg ="Token inválido!";
+    $objResposta->status = false;
+    echo json_encode($objResposta);
 }
-
-echo json_encode($objResposta);
-
 
 ?>
